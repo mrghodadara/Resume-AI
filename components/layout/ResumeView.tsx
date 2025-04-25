8aff3cd67f4a73bbf927671da3e4dcf2a8556853
mrghodadara <vgghodadara2003@gmail.com>
2025-04-26 01:57:16 +0530

    Working on view resume page design creating

--- components/layout/ResumeView.tsx
+++ components/layout/ResumeView.tsx
@@ -9,6 +9,8 @@ import ResumePreview from '@/components/layout/my-resume/ResumePreview';
 import { usePathname } from 'next/navigation';
 import PageWrapper from '@/components/common/PageWrapper';
 import { DownloadIcon, Share2Icon } from 'lucide-react';
+import { Container } from './Container';
+import Footer from './Footer';
 
 const FinalResumeView = ({
   params,
@@ -25,69 +27,77 @@ const FinalResumeView = ({
   };
 
   return (
-    <PageWrapper>
-      <FormProvider params={params}>
-        <div id="no-print">
-          <Header />
-          <div className="my-10 mx-10 md:mx-20 lg:mx-36">
-            {isOwnerView ? (
-              <>
-                <h2 className="text-center text-2xl font-bold">
-                  Congrats! Your ultimate AI-generated resume is ready!
-                </h2>
-                <p className="text-center text-gray-600">
-                  You can now download your resume or share its unique URL with
-                  your friends and family.
-                </p>
-                <p className="text-center text-sm text-gray-500 font-light">
-                  For better print quality, adjust your browser's print
-                  settings: save as PDF, disable headers and footers, set
-                  margins to none, and enable background graphics.
-                </p>
-              </>
-            ) : (
-              <>
-                <h2 className="text-center text-2xl font-bold">
-                  Resume Preview
-                </h2>
-                <p className="text-center text-gray-600">
-                  You are currently viewing a preview of someone else's resume.
-                </p>
-                <p className="text-center text-sm text-gray-500 font-light">
-                  For the ultimate experience, create your own AI-generated
-                  resume.
-                </p>
-              </>
-            )}
-            <div className="flex max-sm:flex-col justify-center gap-8 my-10">
-              <Button
-                className="flex px-12 py-6 gap-2 rounded-full bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-700/30 text-white"
-                onClick={handleDownload}
-              >
-                <DownloadIcon className="size-6" /> Download
-              </Button>
-              <RWebShare
-                data={{
-                  text: 'Hello everyone, check out my resume by clicking the link!',
-                  url: `${path}`,
-                  title: `${formData?.firstName} ${formData?.lastName}'s Resume`,
-                }}
-                onClick={() => console.log('Shared successfully!')}
-              >
-                <Button className="flex px-12 py-6 gap-2 rounded-full bg-slate-200 hover:bg-primary-700/20 focus:ring-4 focus:ring-primary-700/30 text-black">
-                  <Share2Icon className="size-6" /> Share URL
-                </Button>
-              </RWebShare>
-            </div>
-          </div>
+    <>
+      <main className="flex min-h-screen flex-col bg-gray-950 text-white">
+        <Header />
+        <div className="min-h-screen bg-gray-950 text-white pt-16">
+          <Container className="py-8 flex flex-col gap-8">
+            <FormProvider params={params}>
+              <div className="no-print">
+                <div className="my-10 mx-10 md:mx-20 lg:mx-36">
+                  {isOwnerView ? (
+                    <>
+                      <h2 className="text-center text-2xl font-bold">
+                        Congrats! Your ultimate AI-generated resume is ready!
+                      </h2>
+                      <p className="text-center text-gray-600">
+                        You can now download your resume or share its unique URL
+                        with your friends and family.
+                      </p>
+                      <p className="text-center text-sm text-gray-500 font-light">
+                        For better print quality, adjust your browser's print
+                        settings: save as PDF, disable headers and footers, set
+                        margins to none, and enable background graphics.
+                      </p>
+                    </>
+                  ) : (
+                    <>
+                      <h2 className="text-center text-2xl font-bold">
+                        Resume Preview
+                      </h2>
+                      <p className="text-center text-gray-600">
+                        You are currently viewing a preview of someone else's
+                        resume.
+                      </p>
+                      <p className="text-center text-sm text-gray-500 font-light">
+                        For the ultimate experience, create your own
+                        AI-generated resume.
+                      </p>
+                    </>
+                  )}
+                  <div className="flex max-sm:flex-col justify-center gap-8 my-10">
+                    <Button
+                      className="flex px-12 py-6 gap-2 rounded-full bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-700/30 text-white"
+                      onClick={handleDownload}
+                    >
+                      <DownloadIcon className="size-6" /> Download
+                    </Button>
+                    <RWebShare
+                      data={{
+                        text: 'Hello everyone, check out my resume by clicking the link!',
+                        url: `${path}`,
+                        title: `${formData?.firstName} ${formData?.lastName}'s Resume`,
+                      }}
+                      onClick={() => console.log('Shared successfully!')}
+                    >
+                      <Button className="flex px-12 py-6 gap-2 rounded-full bg-slate-200 hover:bg-primary-700/20 focus:ring-4 focus:ring-primary-700/30 text-black">
+                        <Share2Icon className="size-6" /> Share URL
+                      </Button>
+                    </RWebShare>
+                  </div>
+                </div>
+              </div>
+              <div className="px-10 pt-4 pb-16 max-sm:px-5 max-sm:pb-8 print:p-0">
+                <div id="print-area">
+                  <ResumePreview />
+                </div>
+              </div>
+            </FormProvider>
+          </Container>
         </div>
-        <div className="px-10 pt-4 pb-16 max-sm:px-5 max-sm:pb-8 print:p-0">
-          <div id="print-area">
-            <ResumePreview />
-          </div>
-        </div>
-      </FormProvider>
-    </PageWrapper>
+        <Footer />
+      </main>
+    </>
   );
 };
 
