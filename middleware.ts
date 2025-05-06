09bfdb2a3377c65d75df0927726c80b9f51b87ae
mrghodadara <vgghodadara2003@gmail.com>
2025-05-06 15:56:04 +0530

    Some pages as private added in middleware

--- middleware.ts
+++ middleware.ts
@@ -2,14 +2,14 @@ import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
 const isProtectedRoute = createRouteMatcher([
   '/dashboard(.*)',
-  '/profile',
-  '/my-resume/:resumeId/edit',
+  '/profile(.*)',
+  '/my-resume(.*)/edit',
 ]);
 
 export default clerkMiddleware((auth, request) => {
-  // if (isProtectedRoute(request)) {
-  //   auth().protect();
-  // }
+  if (isProtectedRoute(request)) {
+    auth().protect();
+  }
 });
 
 export const config = {
